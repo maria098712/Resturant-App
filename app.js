8319d889240149c0c0563bf9d1ec260fce8ea8ab
maria098712 <103851054+maria098712@users.noreply.github.com>
2023-12-31 13:53:08 +0500

    Add files via upload
    Make sure to add Images.

---
+++ app.js
@@ -0,0 +1,8 @@
+const searchicon2 = document.querySelector('#searchicon2');
+const srchicon2 = document.querySelector('#srchicon2');
+const search1 = document.querySelector('#search1');
+
+searchicon2.addEventListener('click', function () {
+    search2.style.display = 'flex';
+    searchicon2.style.display = 'none';
+});
